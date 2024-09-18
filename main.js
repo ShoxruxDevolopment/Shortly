8455aa3e8eb7b6e5dddc6077cc57d7c7fd565e49
ShoxruxDevolopment <shoxruxrustamovshoxfitnes@gmail.com>
2024-09-18 10:24:07 +0500

    add url-cop-button-click and wrap submit handler

---
+++ main.js
@@ -0,0 +1,27 @@
+const elUrlShortnerForm=document.querySelector('.js-url-shortner-form');
+const elBlockShortner=document.querySelector('.block-shortener');
+const elCopyUrlShortLinkButton=document.querySelector('.js-copy-short-link-button');
+
+
+if(elUrlShortnerForm){
+  elUrlShortnerForm.addEventListener('submit', function(evt){
+    evt.preventDefault();
+
+    elBlockShortner.classList.add('block-shortener-open');
+  })
+}
+if(elBlockShortner){
+  elBlockShortner.addEventListener('click', function(evt){
+    if (evt.target.matches('.js-copy-short-link-button')){
+      evt.target.textContent='Copied!';
+
+      evt.target.classList.add('btn-short-copied');
+
+      setTimeout(function(){
+        evt.target.textContent='Copy';
+        evt.target.classList.remove('btn-short-copied');
+      },1000);
+
+    }
+  });
+}
